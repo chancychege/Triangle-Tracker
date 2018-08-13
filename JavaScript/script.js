7e27d2a169936a473b02507abbf45c234a264734
chancy <chancychege@gmail.com>
2018-08-13 07:19:46 +0300

    add changes to javasciot

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -2,16 +2,7 @@ function getTriangle(){
 var a = parseFloat(prompt("side a"));
 var b = parseFloat(prompt("side b"));
 var c = parseFloat(prompt("side c"));
-/* var determine = function(a, b, c){
-if (a === b && b === c && a === c){
-  alert("Equilateral Triange");
-    }
 
-else if ((a === b && b === a) (a != c && b !== C));
-alert("Isosceles");
-
-}
- determine(); */
 
 
  if(a===b && a===c && b===c){
@@ -28,7 +19,3 @@ alert("Scalar");
 }
 }
 getTriangle();
-
-
-
-Collapse 
